e70919ff014740a207a9218d0d9462b99a9a6b25
mee06618 <bro89977@naver.com>
2021-07-26 13:51:43 +0900

    exit

--- Temp.js
+++ Temp.js
@@ -3,12 +3,14 @@ import { View } from 'react-native';
 import { Text, TouchableRipple } from 'react-native-paper';
 import tw from 'tailwind-react-native-classnames';
 const abc = () => (
-  <TouchableRipple style={tw`bg-blue-100 h-full`}
+    <View style={tw`h-full `}>
+  <TouchableRipple style={tw`w-12 h-12 mr-5 ml-auto mb-48 mt-auto rounded-full bg-red-300`}
     onPress={() => console.log('Pressed')}
     rippleColor="rgba(0, 0, 0, .32)"
   >
-    <Text>Press anywhere</Text>
+    <Text></Text>
   </TouchableRipple>
+  </View>
 );
 
 export default abc;
